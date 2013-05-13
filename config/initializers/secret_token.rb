285c74fdba9238d8deab972ca390f25731f0263f
Josh <jscanish@gmail.com>
2013-05-14 02:44:04 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoppingCart::Application.config.secret_key_base = '499f04d0c8792cd21898350eeaf6aa6355233b5b18d127627ca90704f555861e744e9eae7a39551ab40239b59e316f3bc4318c635a2d44ac098a2e347a13b8c7'
